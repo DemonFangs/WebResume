8325900bdda08fdc1ce38c0e14289f077bae647e
DemonFangs <demon_avi@hotmail.com>
2017-09-30 16:25:13 -0400

    vertical aligned nav-header, added new scrolling design to each section, fixed scrolling offset

--- js/theme.js
+++ js/theme.js
@@ -1,3 +1,35 @@
+var aboutPosition = $('#about').offset().top - 58;
+var skillsPosition = $('#skills').offset().top - 58;
+var experiencePosition = $('#experience').offset().top - 58;
+var linksPosition = $('#links').offset().top - 58;
+
+$(document).scroll(function() {
+  var currentScrollPosition = $(document).scrollTop();
+  if (currentScrollPosition < aboutPosition){
+  	$('.header').css('border-bottom-width', '0px');
+  }else if (currentScrollPosition >= aboutPosition && currentScrollPosition < skillsPosition) {
+    $('#about .header').css('border-bottom-width', '2px');
+    $('#skills .header').css('border-bottom-width', '0px');
+    $('#experience .header').css('border-bottom-width', '0px');
+    $('#links .header').css('border-bottom-width', '0px');
+  } else if (currentScrollPosition >= skillsPosition && currentScrollPosition < experiencePosition) {
+  	$('#about .header').css('border-bottom-width', '0px');
+    $('#skills .header').css('border-bottom-width', '2px');
+    $('#experience .header').css('border-bottom-width', '0px');
+    $('#links .header').css('border-bottom-width', '0px');
+  } else if (currentScrollPosition >= experiencePosition && currentScrollPosition < linksPosition) {
+  	$('#about .header').css('border-bottom-width', '0px');
+    $('#skills .header').css('border-bottom-width', '0px');
+    $('#experience .header').css('border-bottom-width', '2px');
+    $('#links .header').css('border-bottom-width', '0px');
+  } else if (currentScrollPosition >= linksPosition) {
+  	$('#about .header').css('border-bottom-width', '0px');
+    $('#skills .header').css('border-bottom-width', '0px');
+    $('#experience .header').css('border-bottom-width', '0px');
+    $('#links .header').css('border-bottom-width', '2px');
+  }
+});
+
 function changeCSS(cssFile, cssLinkIndex) {
 
 	var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
